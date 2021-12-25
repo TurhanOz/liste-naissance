a066749a83d49f64c3ffffefab94bef60df45286
Turhan Oz <@turhanoz>
2021-12-25 16:45:16 +0100

    [home][header][footer]

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,12 +2,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePage } from './pages/home/home.page';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePage
+    HomePage,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
