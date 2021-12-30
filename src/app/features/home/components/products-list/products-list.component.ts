7d063669be3f85e4f80e045ab234c3a196735b01
Turhan Oz <@turhanoz>
2021-12-30 10:24:37 +0100

    [home][products]

--- src/app/features/home/components/products-list/products-list.component.ts
+++ src/app/features/home/components/products-list/products-list.component.ts
@@ -8,13 +8,18 @@ import { Product } from '../product/product.component';
 })
 export class ProductsListComponent implements OnInit {
   products:Product[] = [
-    {image:"/assets/poussette.jpeg", name:"poussette ultra chère!"},
-    {image:"/assets/poussette.jpeg", name:"poussette ultra chère!"},
-    {image:"/assets/poussette.jpeg", name:"poussette ultra chère!"},
-    {image:"/assets/poussette.jpeg", name:"poussette ultra chère!"},
-    {image:"/assets/poussette.jpeg", name:"poussette ultra chère!"},
-    {image:"/assets/poussette.jpeg", name:"poussette ultra chère!"},
-    {image:"/assets/poussette.jpeg", name:"poussette ultra chère!"}
+    {image:"/assets/poussette.jpeg", name:"poussette"},
+    {image:"/assets/porte-bebe.jpeg", name:"porte-bébé"},
+    {image:"/assets/sac-a-langer.jpeg", name:"sac à langer"},
+    {image:"/assets/lit-de-voyage.jpeg", name:"lit de voyage"},
+    {image:"/assets/siege-auto.jpeg", name:"siège auto"},
+    {image:"/assets/tapis-eveil.jpeg", name:"tapis d'éveil"},
+    {image:"/assets/lit-barreaux-bebe.jpeg", name:"lit à barreaux"},
+    {image:"/assets/gigoteuse.jpeg", name:"gigoteuses"},
+    {image:"/assets/table-a-langer.jpeg", name:"table à langer"},
+    {image:"/assets/biberons.jpeg", name:"biberons"},
+    {image:"/assets/veilleuse.jpeg", name:"veilleuse"},
+    {image:"/assets/vetements.jpeg", name:"vêtements..."},
   ];
 
   constructor() { }
